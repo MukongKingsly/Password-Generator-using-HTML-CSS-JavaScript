fd4dda690dcb799c93a4731c0ddc1f51010a3d74
Mukong <kingmbamu@yahoo.co.uk>
2024-07-27 11:09:53 +0200

    Added setTimeout, comments and changed background image

--- app.js
+++ app.js
@@ -1,120 +1,143 @@
 class PasswordGenerator {
   constructor() {
-    this.characterAmountRange = document.getElementById("characterAmountRange");
-    this.characterAmountNumber = document.getElementById(
-      "characterAmountNumber"
-    );
-    this.includeUppercaseElement = document.getElementById("includeUppercase");
-    this.includeNumbersElement = document.getElementById("includeNumbers");
-    this.includeSymbolsElement = document.getElementById("includeSymbols");
-    this.form = document.getElementById("passwordGeneratorForm");
-    this.passwordDisplay = document.getElementById("passwordDisplay");
-    this.passwordInput = document.getElementById("passwordInput");
-    this.copyButton = document.getElementById("copyButton");
-    this.svgButton = document.getElementById("svg-button");
+    // Initialize elements from the DOM and assign them to this.elements
+    this.elements = {
+      characterAmountRange: document.getElementById("characterAmountRange"),
+      characterAmountNumber: document.getElementById("characterAmountNumber"),
+      includeUppercase: document.getElementById("includeUppercase"),
+      includeNumbers: document.getElementById("includeNumbers"),
+      includeSymbols: document.getElementById("includeSymbols"),
+      form: document.getElementById("passwordGeneratorForm"),
+      passwordDisplay: document.getElementById("passwordDisplay"),
+      copyButton: document.getElementById("copyButton"),
+      svgButton: document.getElementById("svg-button"),
+    };
 
-    this.UPPERCASE_CHAR_CODES = this.arrayFromLowToHigh(65, 90);
-    this.LOWERCASE_CHAR_CODES = this.arrayFromLowToHigh(97, 122);
-    this.NUMBER_CHAR_CODES = this.arrayFromLowToHigh(48, 57);
-    this.SYMBOL_CHAR_CODES = this.arrayFromLowToHigh(33, 47)
-      .concat(this.arrayFromLowToHigh(58, 64))
-      .concat(this.arrayFromLowToHigh(91, 96))
-      .concat(this.arrayFromLowToHigh(123, 126));
+    // Define character code ranges for different character types
+    this.CHAR_CODES = {
+      UPPERCASE: this.arrayFromLowToHigh(65, 90), // A-Z
+      LOWERCASE: this.arrayFromLowToHigh(97, 122), // a-z
+      NUMBER: this.arrayFromLowToHigh(48, 57), // 0-9
+      SYMBOL: [
+        ...this.arrayFromLowToHigh(33, 47), // Symbols ! to /
+        ...this.arrayFromLowToHigh(58, 64), // Symbols : to @
+        ...this.arrayFromLowToHigh(91, 96), // Symbols [ to `
+        ...this.arrayFromLowToHigh(123, 126), // Symbols { to ~
+      ],
+    };
 
-    this.characterAmountNumber.addEventListener(
-      "input",
-      this.syncCharacterAmount.bind(this)
-    );
-    this.characterAmountRange.addEventListener(
-      "input",
-      this.syncCharacterAmount.bind(this)
-    );
-    this.form.addEventListener("submit", this.handleFormSubmit.bind(this));
-    this.copyButton.addEventListener(
-      "click",
-      this.handleCopyButtonClick.bind(this)
-    );
-    this.svgButton.addEventListener(
-      "click",
-      this.handleSvgButtonClick.bind(this)
-    );
-    window.addEventListener("load", this.updatePassword.bind(this));
+    // Bind event listeners to DOM elements
+    this.bindEvents();
+    // Update password display on initial load
+    window.addEventListener("load", () => this.updatePassword());
   }
 
-  updatePassword() {
-    const characterAmount = this.characterAmountNumber.value;
-    const includeUppercase = this.includeUppercaseElement.checked;
-    const includeNumbers = this.includeNumbersElement.checked;
-    const includeSymbols = this.includeSymbolsElement.checked;
-    const password = this.generatePassword(
-      characterAmount,
+  // Bind various event listeners to their respective handlers
+  bindEvents() {
+    const {
+      characterAmountNumber,
+      characterAmountRange,
+      form,
+      copyButton,
+      svgButton,
+    } = this.elements;
+
+    characterAmountNumber.addEventListener("input", this.syncCharacterAmount);
+    characterAmountRange.addEventListener("input", this.syncCharacterAmount);
+    form.addEventListener("submit", this.handleFormSubmit);
+    copyButton.addEventListener("click", this.handleCopyButtonClick);
+    svgButton.addEventListener("click", this.handleSvgButtonClick);
+  }
+
+  // Update displayed password whenever the form input changes
+  updatePassword = () => {
+    const {
+      characterAmountNumber,
       includeUppercase,
       includeNumbers,
-      includeSymbols
+      includeSymbols,
+      passwordDisplay,
+    } = this.elements;
+    const characterAmount = characterAmountNumber.value;
+    const password = this.generatePassword(
+      characterAmount,
+      includeUppercase.checked,
+      includeNumbers.checked,
+      includeSymbols.checked
     );
-    this.passwordDisplay.innerText = password;
-  }
+    passwordDisplay.innerText = password;
+  };
 
-  handleFormSubmit(e) {
+  // Handle form submission, prevent default form behavior, and update password
+  handleFormSubmit = (e) => {
     e.preventDefault();
     this.updatePassword();
-  }
+    this.handleResetButton();
+  };
 
-  generatePassword(
+  // Generate a password based on selected options
+  generatePassword = (
     characterAmount,
     includeUppercase,
     includeNumbers,
     includeSymbols
-  ) {
-    let charCodes = this.LOWERCASE_CHAR_CODES;
-    if (includeUppercase)
-      charCodes = charCodes.concat(this.UPPERCASE_CHAR_CODES);
-    if (includeSymbols) charCodes = charCodes.concat(this.SYMBOL_CHAR_CODES);
-    if (includeNumbers) charCodes = charCodes.concat(this.NUMBER_CHAR_CODES);
+  ) => {
+    const charCodes = [
+      ...this.CHAR_CODES.LOWERCASE,
+      ...(includeUppercase ? this.CHAR_CODES.UPPERCASE : []),
+      ...(includeSymbols ? this.CHAR_CODES.SYMBOL : []),
+      ...(includeNumbers ? this.CHAR_CODES.NUMBER : []),
+    ];
 
-    const passwordCharacters = [];
-    for (let i = 0; i < characterAmount; i++) {
+    return Array.from({ length: characterAmount }, () => {
       const characterCode =
         charCodes[Math.floor(Math.random() * charCodes.length)];
-      passwordCharacters.push(String.fromCharCode(characterCode));
-    }
-    return passwordCharacters.join("");
-  }
+      return String.fromCharCode(characterCode);
+    }).join("");
+  };
 
-  arrayFromLowToHigh(low, high) {
-    const array = [];
-    for (let i = low; i <= high; i++) {
-      array.push(i);
-    }
-    return array;
-  }
+  // Utility function to create an array of numbers within a specified range
+  arrayFromLowToHigh = (low, high) =>
+    Array.from({ length: high - low + 1 }, (_, i) => low + i);
 
-  syncCharacterAmount(e) {
-    const value = e.target.value;
-    this.characterAmountNumber.value = value;
-    this.characterAmountRange.value = value;
-  }
+  // Sync input range and number fields to have the same value
+  syncCharacterAmount = (e) => {
+    const { value } = e.target;
+    this.elements.characterAmountNumber.value = value;
+    this.elements.characterAmountRange.value = value;
+  };
 
-  handleCopyButtonClick() {
-    this.copyToClipboard(this.passwordDisplay.innerText);
-    this.copyButton.innerText = "Password copied to clipboard";
-    this.copyButton.classList.add("text-copied-btn");
-  }
+  // Handle copy button click event, copy password to clipboard, and display message
+  handleCopyButtonClick = () => {
+    const { passwordDisplay, copyButton } = this.elements;
+    this.copyToClipboard(passwordDisplay.innerText);
+    copyButton.innerText = "Password copied to clipboard";
+    copyButton.classList.add("text-copied-btn");
 
-  handleSvgButtonClick() {
+    // Reset copy button text and class after 5 seconds
+    setTimeout(this.handleResetButton, 5000);
+  };
+
+  // Reset copy button text and class
+  handleResetButton = () => {
+    const { copyButton } = this.elements;
+    copyButton.innerText = "Copy to Clipboard";
+    copyButton.classList.remove("text-copied-btn");
+  };
+
+  // Handle SVG button click event to reload the page
+  handleSvgButtonClick = () => {
     window.location.reload();
-  }
+  };
 
-  copyToClipboard(text) {
+  // Copy text to clipboard using the Clipboard API
+  copyToClipboard = (text) => {
     navigator.clipboard.writeText(text).then(
-      function () {
-        return;
-      },
-      function (err) {
-        console.error("Could not copy text: ", err);
-      }
+      () => {},
+      (err) => console.error("Could not copy text: ", err)
     );
-  }
+  };
 }
 
+// Instantiate the PasswordGenerator class when the page loads
 const passwordGenerator = new PasswordGenerator();
